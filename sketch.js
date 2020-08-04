956fd7c804487de6cbe77e04b2a5e22878389c6c
PackOfDeepak <68823003+PackOfDeepak@users.noreply.github.com>
2020-08-04 11:52:57 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,40 @@
-var car,wall;
-
-var speed,weight;
-
-var deformation;
+var fixedRect,movingRect;
 
 function setup() {
-  createCanvas(1600,400);
-  speed = random(55,90);
-  weight = random(400,1500);
-
-  car = createSprite(50,200,50,50);
-  wall = createSprite(1500,200,60,height/2);
-  wall.shapeColor = 80,80,80;
-  car.velocityX = speed;
+  createCanvas(1200,800);
+ fixedRect =  createSprite(200, 200, 50, 80);
+ movingRect = createSprite(400,200,80,30);
+ fixedRect.shapeColor = "green";
+ movingRect.shapeColor = "green";
+ fixedRect.debug = true;
+ movingRect.debug = true;
 }
 
 function draw() {
-  background(255,255,255); 
-  if(wall.x - car.x < wall.width/2 + car.width/2){
-    deformation = 0.5*weight*speed*speed;
-    car.velocityX = 0;
-    car.depth = wall.depth+2;
-    if(deformation<100){
-      car.shapeColor = "green";
-
-    }
-    if(deformation > 100 && deformation < 180){
-      car.shapeColor = "yellow";
-    }
-    if(deformation>180){
-      car.shapeColor = "red";
-    }
-
-
+  background(0,0,0);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY; 
+  if(isTouching(movingRect,fixedRect)){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }else{
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
   }
   
-
-
+  
   drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2 && object2.x - object1.x < object2.width/2 + object1.width/2 && object1.y - object2.y < object2.height/2 + object1.height/2 && object2.y - object1.y < object2.height/2 + object1.height/2){
+    return true;
+  }
+  else{
+    return false;
+  }
+  
+  
+  
+ 
 }
